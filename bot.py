b2d8cc4eba3416810f121f338b962ab6ba7d2896
Symmetry-P <38298355+Symmetry-P@users.noreply.github.com>
2018-04-17 18:09:16 -0400

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,25 @@
+import discord
+import asyncio
+import math
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+    
+check = 'I pledge to adhere to the regulations of this server: Philosophorum Disputationibusis. In addition to that, I will also engage in intellectually stimulating philosophical discussions during my time on this server.'
+
+@client.event
+async def on_message(message):
+    if message.content == check:
+        await client.add_roles(message.author, discord.utils.get(message.server.roles, name='Verified'))
+    
+@client.event
+async def on_member_join(member):
+    await client.send_message(discord.utils.get(member.server.channels, name='station'), 'Welcome to the *Philosophorum Disputationibusis* Server! ' + member.mention + '\n\nRead the ' + discord.utils.get(member.server.channels, name='regulations').mention + ' and ' + discord.utils.get(member.server.channels, name='information').mention + ' channels. Once you have done so, type (or copy) `' + check + '` \n\nIn doing so you will be verified and will be able to proceed to the main area of the server.')        
+
+client.run('NDM1OTAxMjUyMDk4ODUwODI3.DbfsVg.TUtaTQJpjmnkeFJXSYCb5B4yZJw')
